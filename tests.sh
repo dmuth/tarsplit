d80df0a96eab3d51a1fe8e8f558caee36441405a
Douglas Muth <dmuth@dmuth.org>
2020-11-24 01:19:19 -0500

    Finished first draft of testing script!

--- tests.sh
+++ tests.sh
@@ -7,33 +7,66 @@
 # Errors are fatal
 set -e
 
-#
-# Make a temp directory and hop right in there!
-#
+TARBALL="test-tarball.tgz"
+
 DIR=$(pwd)
 BIN=${DIR}/bin
-TMP=$(mktemp -d)
-pushd ${TMP} > /dev/null
+TARSPLIT=${DIR}/tarsplit
 
 #
 # Create our test tarball and get the SHA1
 #
 ${BIN}/create-test-tarball.sh
-SHA1=$(${BIN}/sha1-from-tarball.sh test-tarball.tgz)
-echo "# Our SHA1 to match against: ${SHA1}"
+SHA1=$(${BIN}/sha1-from-tarball.sh ${TARBALL})
+echo "##### Our SHA1 to match against: ${SHA1} #####"
 
 
-# TODO:
-# X Temp directory
-# X Create test tarball
-# X Get SHA1
-# - Split tarball 2 ways
-# - Write function to untar list of files
-# - Get SHA1 from files and test
-# - Split tarball 3, 4, 5, and 10 ways
-# - Get SHA1 from files and test
+#
+# Split our tarball into a number of parts, extract those parts, and check SHA1 values.
+#
+function split_and_test() {
+
+	NUM_PARTS=$1
+
+	TMP=$(mktemp -d)
+	pushd ${TMP} > /dev/null
+
+	echo "# Splitting our test tarball into ${NUM_PARTS} parts..."
+	${TARSPLIT} ${DIR}/${TARBALL} $NUM_PARTS > /dev/null
+
+	echo "# Extracting parts we just created..."
+	for FILE in test-tarball.tgz-part-*
+	do
+		tar xfz ${FILE}
+	done
+
+	echo "# Getting our SHA1 from extracted files..."
+	SHA1_CHECK=$(${BIN}/sha1-from-directory.sh tarball-root-dir)
+
+	if test $SHA1 == $SHA1_CHECK
+	then
+		echo "# PASS! ($SHA1 == $SHA1_CHECK)"
+	else
+		echo "# FAIL! ($SHA1 != $SHA1_CHECK)"
+		exit 1
+	fi
+
+	# Leave the directory and cleanup
+	popd > /dev/null
+	rm -rf ${TMP}
+
+} # End of split_and_test()
 
 
-# create-test-tarball.sh
-# sha1-from-directory.sh
+#
+# Run our tests
+#
+split_and_test 2
+split_and_test 3
+split_and_test 4
+split_and_test 5
+split_and_test 10
+
+echo "OK: Done!"
+
 
