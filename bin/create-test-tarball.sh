7572b30b6c6fe13a94a73b158e2e129bd765c2a3
Douglas Muth <dmuth@dmuth.org>
2020-11-28 15:22:30 -0500

    Added symlink creation

--- bin/create-test-tarball.sh
+++ bin/create-test-tarball.sh
@@ -7,6 +7,7 @@
 set -e
 
 NUM_FILES_AND_DIRS=5
+#NUM_FILES_AND_DIRS=2 # Testing/Debugging
 
 # Our root directory for the tarball
 DIR="tarball-root-dir"
@@ -30,13 +31,19 @@ for I in $(seq ${NUM_FILES_AND_DIRS})
 do
 	mkdir -p ${DIR}/${I}
 
+	#
+	# Make a file in this directory.
+	#
 	TARGET="${DIR}/file-${I}.data"
 	if test ! -f "${TARGET}"
 	then
 		dd if=/dev/zero of="${TARGET}" bs=1m count=1 2> /dev/null
 	fi
 
-	for J in $(seq 5)
+	#
+	# Make our files in each sub-directory
+	#
+	for J in $(seq ${NUM_FILES_AND_DIRS})
 	do
 		mkdir -p ${DIR}/${I}/${J}
 		
@@ -45,8 +52,17 @@ do
 		then
 			dd if=/dev/zero of="${TARGET}" bs=1m count=1 2> /dev/null
 		fi
-
 	done
+
+	#
+	# Make a symlink to the previous number.
+	# I am aware that this will result in the first symlink being broken, and
+	# that is intentional, as I want broken symlinks to created/tested.
+	#
+	SOURCE="${DIR}/${I}/file-${I}.link"
+	TARGET="../file-$(( I - 1 )).data" 
+
+	ln -sf ${TARGET} ${SOURCE} 
 
 done
 
